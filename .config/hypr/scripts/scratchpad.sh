7d5faf1288b433dcec832814772767dae0d0db40
mhbkamaei <mh.b.kamaei.com>
2024-02-01 17:14:00 +0330

    fix scrachpad launch bug

--- .config/hypr/scripts/scratchpad.sh
+++ .config/hypr/scripts/scratchpad.sh
@@ -3,7 +3,7 @@
 class="$(hyprctl clients | grep scpad)"
 
 if [ -z "$class" ]; then
-	kitty --class scpad --hold bash -c "hyprctl dispatch movetoworkspace special:magic &&
+	kitty --class scpad --hold bash -c "hyprctl dispatch movetoworkspacesilent special:magic &&
 	                                    hyprctl dispatch togglespecialworkspace magic &&
                                       clear &&
                                       tmux att -t scpad || tmux new -s scpad"
