7ae6a6043c644eb1593da586ff44eb6eb9b72e99
mhbkamaei <mh.b.kamaei.com>
2024-03-02 16:05:43 +0330

    feat: Add key bind cheat sheet

--- .local/bin/fzf-nvim.sh
+++ .local/bin/fzf-nvim.sh
@@ -8,16 +8,26 @@ _fzf_vim_fzf() {
 }
 
 _fzf_vim_config() {
-	fd . ~/.config -t f | _fzf_vim_fzf | xargs -r echo "nvim"
+	read -e
+	fd . ~/.dotfiles/.config/ -t f | _fzf_vim_fzf | xargs -r echo "nvim"
+}
+
+_fzf_vim_local() {
+	read -e
+	fd . ~/.dotfiles/.local/ -t f | _fzf_vim_fzf | xargs -r echo "nvim"
+}
+
+_fzf_vim_help() {
+	read -e
+	cat ~/.config/key_bindings/keys | _fzf_vim_fzf >/dev/null
 }
 
 __fzf_vim_init() {
 	bind '"\er": redraw-current-line'
 	local o
 	for o in "$@"; do
-		bind -m emacs-standard '"\C-g'${o:0:1}'": "`_fzf_vim_'$o'`\e\C-e\er\C-m"'
+		bind -m emacs-standard '"\C-f'${o:0:1}'": "`_fzf_vim_'$o'`\e\C-e\er\C-m"'
 	done
 }
-
-__fzf_vim_init config
+__fzf_vim_init config local help
 # -----------------------------------------------------------------------------
